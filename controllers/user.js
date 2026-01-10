6bd6343721ab9092ed80e0f19b95464dd01a8a19
Anusha12-vijay <anushavijay1202@gmail.com>
2026-01-10 19:29:32 +0530

    feat: added signup login and logout authentication controllers using JWT

---
+++ controllers/user.js
@@ -0,0 +1,75 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import User from "../models/user.js"
+import {inngest} from "../inngest/client.js"
+
+
+
+export const signup = async (req,res) => {
+    const {email,password,skills=[]} = req.body
+    try{
+      const hashed= await bcrypt.hash(password,10)
+      const user =await User.create({email,password: hashed,skills})
+
+      //Fire inngest event
+
+      await inngest.send({
+        name:"user/signup",
+        data:{
+            email
+        }
+      });
+
+      const token = jwt.sign(
+        {_id:user._id,role: user.role},process.env.JWT_SECRET);
+
+      res.json({user,token})
+
+
+    }catch(error){
+        res.status(500).json({ error: "Signup failed",details: error.message});
+        
+
+    }
+
+}
+
+export const login = async(req, res) => {
+  const {email,password} = req.body
+
+  try{
+    const user =await User.findOne({email})
+    if(!user) return res.status(401).json({error:"User not found"})
+
+      const isMatch = await bcrypt.compare(password,user.password);
+
+      if(!isMatch){
+        return res.status(401).json({error:"Invalid credentials"})
+      }
+      const token = jwt.sign(
+        {_id:user._id,role: user.role},process.env.JWT_SECRET);
+
+      res.json({user,token})
+
+  }catch(error){
+    res.status(500).json({ error: "Login failed",details: error.message});
+
+  }
+
+}
+
+//this is kind of a handshake. 
+export const logout =async(req,res) =>{
+  try{
+    const token = req.headers.authorization.split(" ")[1]
+    if(!token) return res.status(401).json({error: "Unauthorised"})
+    jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
+  if(err) return res.status(401).json({error: "Unauthorised"})})
+    res.json({message:"Logout successfully"})
+
+
+  }catch(error){
+    res.status(500).json({ error: "Logout failed",details: error.message});
+
+  }
+}
