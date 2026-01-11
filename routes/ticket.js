e2284115b587928c91371919cd01a9dc47619261
Anusha12-vijay <anushavijay1202@gmail.com>
2026-01-11 22:16:55 +0530

    feat: add ticket controllers with event-driven AI processing

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,6 +1,12 @@
 import express from "express"
+import {authenticate} from "../middlewares/auth.js"
+import { createticket, getTicket, getTickets } from "../controllers/ticket.js"
+
 
 const router =express.Router()
 
+router.get("/",authenticate,getTickets)
+router.get("/:id",authenticate,getTicket)
+router.post("/",authenticate,createticket);
 
 export default router
