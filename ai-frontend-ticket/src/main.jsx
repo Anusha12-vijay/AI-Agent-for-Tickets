a9f1e05acd8ae50b146999be5b0e1c1ec006456a
Anusha12-vijay <anushavijay1202@gmail.com>
2026-01-12 03:17:00 +0530

    implemented protected routes in React using a custom authentication wrapper to control access based on login state.

--- ai-frontend-ticket/src/main.jsx
+++ ai-frontend-ticket/src/main.jsx
@@ -1,10 +1,66 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Route,Routes } from 'react-router-dom'
+import CheckAuth from './components/check_auth.jsx'
+import Tickets from './pages/tickets.jsx'
+import TicketDetails from './pages/ticket.jsx'
+import Signup from './pages/signup.jsx'
+import Admin from './pages/admin.jsx'
+import Login from './pages/login.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route
+      path='/'
+      element={
+        <CheckAuth protected={true}>
+          <Tickets />
+
+        </CheckAuth>
+      }
+      />
+       <Route
+      path='/tickets/:id'
+      element={
+        <CheckAuth protected={true}>
+          <TicketDetails />
+
+        </CheckAuth>
+      }
+      />
+       <Route
+      path='/signup'
+      element={
+        <CheckAuth protected={false}>
+          <Signup />
+
+        </CheckAuth>
+      }
+      />
+             <Route
+      path='/login'
+      element={
+        <CheckAuth protected={false}>
+          <Login />
+
+        </CheckAuth>
+      }
+      />
+      
+       <Route
+      path='/admin'
+      element={
+        <CheckAuth protected={true}>
+          <Admin />
+
+        </CheckAuth>
+      }
+      />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
